5893c98e2d42c0d0efce91ab03237410a9a3f741
jvazlop <jvazquezl@iesrafaelalberti.es>
2026-04-09 10:58:04 +0200

    main beta

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,41 @@
 package org.iesra
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.io.File
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+
+    val parseador = Parseador()
+    val lineas = File("sample_app.log").readLines()
+
+    val logsValidos = mutableListOf<EntradaLog>()
+    var invalidas = 0
+
+    for (linea in lineas) {
+        val log = parseador.parse(linea)
+
+        if (log != null) {
+            logsValidos.add(log)
+        } else {
+            invalidas++
+        }
     }
+
+    println("Total líneas: ${lineas.size}")
+    println("Líneas procesadas: ${lineas.size}")
+    println("Líneas válidas: ${logsValidos.size}")
+    println("Líneas inválidas: $invalidas")
+
+    val info = logsValidos.count { it.level == LogLevel.INFO }
+    val warning = logsValidos.count { it.level == LogLevel.WARNING }
+    val error = logsValidos.count { it.level == LogLevel.ERROR }
+
+    println("INFO: $info")
+    println("WARNINGS: $warning")
+    println("ERRORS: $error")
+
+    val primera = logsValidos.minByOrNull { it.timestamp }?.timestamp
+    val ultima = logsValidos.maxByOrNull { it.timestamp }?.timestamp
+
+    println("Primera fecha: $primera")
+    println("Última fecha: $ultima")
 }
